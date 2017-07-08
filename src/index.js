5e0d3b72129dd010f0c9584a4e5713678059db1d
ressiver <diospyros.digyna@gmail.com>
2017-07-08 12:44:12 +0300

    change index.js and done all code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
+var textField = $('.message-field');
+var textFormatted = $('.message-formatted');
+
+function showTextFormatted(value) {
+    return value.trim().toLowerCase().replace(/\s+/g, ' ');
+}
+
+textField.on('input', function () {
+    var value = textField.val();
+    var doneFormatted = showTextFormatted(value);
+    textFormatted.text(doneFormatted);
+});
+
 console.log($)
